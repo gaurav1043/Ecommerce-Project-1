a932db4d9c1a85314d46bb6badeb4d859a7e6f91
gaurav madhavi <gauravaan21@gmail.com>
2023-01-09 00:03:59 +0530

    Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import ProductList from "./pages/ProductList";
 import ProductPage from "./pages/ProductPage";
+import Register from "./pages/Register";
 
 function App() {
-  return <ProductPage />;
+  return <Home />;
 }
 
 export default App;
